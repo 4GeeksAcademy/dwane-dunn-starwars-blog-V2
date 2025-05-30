e158395e97f70893196bec8fe50093de315b5af8
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-30 22:45:15 +0000

    scafold the fetch

--- src/pages/PeopleList.jsx
+++ src/pages/PeopleList.jsx
@@ -1,10 +1,28 @@
 // Import necessary components from react-router-dom and other parts of the application.
 import { Link } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer";  // Custom hook for accessing the global state.
+import { useEffect } from "react";
 
 export const PeopleList = () => {
   // Access the global state and dispatch function using the useGlobalReducer hook.
   const { store, dispatch } = useGlobalReducer()
+
+
+const fetchPeople = async () => {
+  try {
+      // const api_url = `https://www.swapi.tech/api/people?page=1&limit=8&expanded=true`;
+      const response = await fetch(`{api_url}`);
+      const result = await response.json();
+      // dispatch the result
+  } catch (error){
+    // set error if there was one
+  } finally {
+    // run anything else here
+  }
+
+  useEffect(()=> {
+    fetchPeople();
+  }, []);
 
   return (
     <div className="container">
