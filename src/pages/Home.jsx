5ee8e91b88549775cd865ffbb08bfd326882b8f1
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-31 13:09:01 +0000

    remove rigo image import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,19 @@
-// import rigoImageUrl from "../assets/img/rigo-baby.jpg";
-import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import useGlobalReducer from '../hooks/useGlobalReducer.jsx';
 // My Custom Pages
-import { PeopleList } from "./PeopleList.jsx";
-import {Planets} from './Planets.jsx'
-import { VehicleList } from "./VehicleList.jsx";
+import { PeopleList } from './PeopleList.jsx';
+import { Planets } from './Planets.jsx';
+import { VehicleList } from './VehicleList.jsx';
 
 export const Home = () => {
+  const { store, dispatch } = useGlobalReducer();
 
-  const {store, dispatch} =useGlobalReducer()
+  return (
+    <div className="text-center mt-5">
+      <h1>Starwars Blog List - Yes?</h1>
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Starwars Blog List</h1>
-			
-			<PeopleList/>
-			<Planets/>
-			<VehicleList/>
-		</div>
-	);
-}; 
+      <PeopleList />
+      <Planets />
+      <VehicleList />
+    </div>
+  );
+};
