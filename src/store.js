a3fc3277445e0fcd2c8f82553a6f42563e4cb34e
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-31 12:51:05 +0000

    fix spacing in store

--- src/store.js
+++ src/store.js
@@ -1,47 +1,45 @@
-export const initialStore=()=>{
-  return{
+export const initialStore = () => {
+  return {
     message: null,
     todos: [
       {
         id: 1,
-        title: "Make the bed",
+        title: 'Make the bed',
         background: null,
       },
       {
         id: 2,
-        title: "Do my homework",
+        title: 'Do my homework',
         background: null,
-      }
-
+      },
     ],
     people: [],
     planets: [],
     vehicles: [],
-
-  }
-}
+  };
+};
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
+  switch (action.type) {
     case 'set_people':
       return {
         ...store,
-        people:action.payload
-      }
+        people: action.payload,
+      };
 
     case 'set_planets':
       return {
         ...store,
-        planets: action.payload
-      }
+        planets: action.payload,
+      };
 
     case 'set_vehicles':
       return {
         ...store,
-        vehicles: action.payload
-      }
-    
+        vehicles: action.payload,
+      };
+
     default:
       throw Error('Unknown action.');
-  }    
+  }
 }
