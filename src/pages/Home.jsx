128ef716063a87cee4023be6f8f6078959e5a34e
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-31 16:16:09 +0000

    build the 3 fetch functions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,9 @@ import { VehicleList } from './VehicleList.jsx';
 export const Home = () => {
   const { store, dispatch } = useGlobalReducer();
   const [loading, setLoading] = useState(true);
+
+  // Function to fetch people data from the Star Wars API
+
   const fetchPeople = async () => {
     try {
       const api_url = `https://www.swapi.tech/api/people?page=1&limit=8&expanded=true`;
@@ -21,13 +24,53 @@ export const Home = () => {
     } catch (error) {
       // set error if there was one
     } finally {
-      setLoading(false);
+      // setLoading(false);
+    }
+  };
+
+  // Function to fetch planets data from the Star Wars API
+  const fetchPlanets = async () => {
+    try {
+      const api_url = `https://www.swapi.tech/api/planets?page=1&limit=8&expanded=true`;
+      const response = await fetch(api_url);
+      const result = await response.json();
+      // console.log('result:', result.results);
+      dispatch({
+        type: 'set_planets',
+        payload: result.results,
+      });
+    } catch (error) {
+      // set error if there was one
+    } finally {
+      // setLoading(false);
+    }
+  };
+
+  // Function to fetch vehicles data from the Star Wars API
+  const fetchVehicles = async () => {
+    try {
+      const api_url = `https://www.swapi.tech/api/vehicles?page=1&limit=8&expanded=true`;
+      const response = await fetch(api_url);
+      const result = await response.json();
+      // console.log('result:', result.results);
+      dispatch({
+        type: 'set_vehicles',
+        payload: result.results,
+      });
+    } catch (error) {
+      // set error if there was one
+    } finally {
+      // setLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchPeople();
+    fetchPeople(); // Get the people data
+    fetchPlanets(); // Get the planets data
+    fetchVehicles(); // Get the vehicles data
     console.log('store.people:', store.people);
+    console.log('store.planets:', store.planets);
+    console.log('store.vehicles:', store.vehicles);
   }, []);
 
   return (
