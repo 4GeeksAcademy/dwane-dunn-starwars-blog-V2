9ad0956752724510e5948b051337ee537cc47956
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-01 13:18:46 +0000

    correct missed export

--- src/pages/PlanetList.jsx
+++ src/pages/PlanetList.jsx
@@ -2,7 +2,7 @@
 import { Link } from 'react-router-dom';
 import useGlobalReducer from '../hooks/useGlobalReducer'; // Custom hook for accessing the global state.
 
-export const VehicleList = () => {
+export const PlanetList = () => {
   // Access the global state and dispatch function using the useGlobalReducer hook.
   const { store, dispatch } = useGlobalReducer();
 
