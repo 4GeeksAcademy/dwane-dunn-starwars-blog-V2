8dc94d500651012d186b56f232023ceaffdfab38
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-02 18:56:36 +0000

    import react icon

--- src/pages/PlanetList.jsx
+++ src/pages/PlanetList.jsx
@@ -1,6 +1,7 @@
 // Import necessary components from react-router-dom and other parts of the application.
 import { Link } from 'react-router-dom';
 import useGlobalReducer from '../hooks/useGlobalReducer'; // Custom hook for accessing the global state.
+import { FaRegHeart } from 'react-icons/fa';
 
 export const PlanetList = () => {
   // Access the global state and dispatch function using the useGlobalReducer hook.
